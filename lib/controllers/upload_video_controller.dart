83fe4ed53830fa2b86050d79962409943d3c7e0c
Hanrocky <hanrocky.halim@gmail.com>
2023-02-03 22:37:33 +0700

    Video Upload Success

--- lib/controllers/upload_video_controller.dart
+++ lib/controllers/upload_video_controller.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 import 'package:tiktok_clone/constant.dart';
 import 'package:video_compress/video_compress.dart';
 
+import '../models/video.dart';
+
 class UploadVideoController extends GetxController {
   _compressVideo(String videoPath) async {
     final compressedVideo = await VideoCompress.compressVideo(
@@ -16,7 +18,21 @@ class UploadVideoController extends GetxController {
   Future<String> _uploadVideoToStorage(String id, String videoPath) async {
     Reference ref = firebaseStorage.ref().child('videos').child(id);
 
-    UploadTask uploadTask = ref.putFile(await _compressVideo(videoPath));
+    UploadTask uploadTask = ref.putFile(await _compressVideo(videoPath),
+        SettableMetadata(contentType: 'video/mp4'));
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  _getThumbnail(String videoPath) async {
+    final thumbnail = await VideoCompress.getFileThumbnail(videoPath);
+    return thumbnail;
+  }
+
+  Future<String> _uploadImageToStorage(String id, String videoPath) async {
+    Reference ref = firebaseStorage.ref().child('thumbnails').child(id);
+    UploadTask uploadTask = ref.putFile(await _getThumbnail(videoPath));
     TaskSnapshot snap = await uploadTask;
     String downloadUrl = await snap.ref.getDownloadURL();
     return downloadUrl;
@@ -31,7 +47,29 @@ class UploadVideoController extends GetxController {
       // get id
       var allDocs = await firestore.collection('videos').get();
       int len = allDocs.docs.length;
-      _uploadVideoToStorage("Video $len", videoPath);
-    } catch (e) {}
+      String videoUrl = await _uploadVideoToStorage("Video $len", videoPath);
+      String thumbnail = await _uploadImageToStorage("Video $len", videoPath);
+
+      Video video = Video(
+        username: (userDoc.data()! as Map<String, dynamic>)['name'],
+        uid: uid,
+        id: "Video $len",
+        likes: [],
+        commentCount: 0,
+        shareCount: 0,
+        songName: songName,
+        caption: caption,
+        videoUrl: videoUrl,
+        thumbnail: thumbnail,
+        profilePhoto: (userDoc.data()! as Map<String, dynamic>)['profilePhoto'],
+      );
+      await firestore
+          .collection('videos')
+          .doc('Video $len')
+          .set(video.toJson());
+      Get.back();
+    } catch (e) {
+      Get.snackbar('Error uploading video', e.toString());
+    }
   }
 }
--- lib/views/screens/confirm_screen.dart
+++ lib/views/screens/confirm_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/controllers/upload_video_controller.dart';
 import 'package:tiktok_clone/views/widgets/text_input_field.dart';
 import 'package:video_player/video_player.dart';
 
@@ -17,8 +19,11 @@ class ConfirmScreen extends StatefulWidget {
 
 class _ConfirmScreenState extends State<ConfirmScreen> {
   late VideoPlayerController controller;
-  TextEditingController songController = TextEditingController();
-  TextEditingController captionController = TextEditingController();
+  final TextEditingController _songController = TextEditingController();
+  final TextEditingController _captionController = TextEditingController();
+
+  UploadVideoController uploadVideoController =
+      Get.put(UploadVideoController());
 
   @override
   void initState() {
@@ -58,7 +63,7 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
                     margin: const EdgeInsets.symmetric(horizontal: 10),
                     width: MediaQuery.of(context).size.width - 20,
                     child: TextInputField(
-                        controller: songController,
+                        controller: _songController,
                         labelText: 'Song Name',
                         icon: Icons.music_note),
                   ),
@@ -69,7 +74,7 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
                     margin: const EdgeInsets.symmetric(horizontal: 10),
                     width: MediaQuery.of(context).size.width - 20,
                     child: TextInputField(
-                        controller: captionController,
+                        controller: _captionController,
                         labelText: 'Caption',
                         icon: Icons.closed_caption),
                   ),
@@ -77,7 +82,10 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
                     height: 10,
                   ),
                   ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () => uploadVideoController.uploadVideo(
+                          _songController.text,
+                          _captionController.text,
+                          widget.videoPath),
                       child: const Text(
                         'Share!',
                         style: TextStyle(
