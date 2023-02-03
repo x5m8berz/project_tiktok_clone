1f7e99b22decd253210ad5ee793e8b9ea1cc6835
Hanrocky <hanrocky.halim@gmail.com>
2023-02-03 12:37:32 +0700

    Added bottom nav bar

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constant.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../widgets/custom_icon.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIdx = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: backgroundColor,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 30),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 30),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: CustomIcon(),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 30),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Home Screen'),
+      ),
+    );
   }
 }
